7491ac3da82a2b0279136381753cbff7b3a0f52d
Byron Omondi <omondibyron25@gmail.com>
2023-05-22 12:54:27 +0300

    complete assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            return
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+if __name__ == "__main__":
+    book = Book("Sample Book", "100")
+    book.turn_page()
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+            return
+        self.brand = brand
+        self.size = size
+        self.condition = 'New'
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = 'New'
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
     pass
